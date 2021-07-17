518b33599acb655932da18fe171cb78cf838d7b0
jwfelipee <arthur993897437@gmail.com>
2021-07-17 15:26:39 -0300

    link da vercel para o BFF

--- pages/index.js
+++ pages/index.js
@@ -225,7 +225,7 @@ export async function getServerSideProps(context) {
   const cookies = nookies.get(context)
   const token = cookies.USER_TOKEN
 
-  const { isAuthenticated } = await fetch('http://localhost:3000/api/auth', {
+  const { isAuthenticated } = await fetch('https://alurakut-jwfelipee.vercel.app/api/auth', {
     headers: {
       Authorization: token
     }
